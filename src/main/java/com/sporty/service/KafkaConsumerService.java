ccc447fdef4e68392164b7ca86b66a0eae1b5347
dinomov <dinomov@gmail.com>
2026-04-06 01:12:08 +0400

    remove manual acknowledge

--- src/main/java/com/sporty/service/KafkaConsumerService.java
+++ src/main/java/com/sporty/service/KafkaConsumerService.java
@@ -7,7 +7,6 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
-import org.springframework.kafka.support.Acknowledgment;
 import org.springframework.retry.annotation.Backoff;
 import org.springframework.retry.annotation.Retryable;
 import org.springframework.stereotype.Service;
@@ -35,7 +34,7 @@ public class KafkaConsumerService {
             maxAttempts = 3,
             backoff = @Backoff(delay = 2000)
     )
-    public void consume(String message, Acknowledgment ack) {
+    public void consume(String message) {
         try {
             JsonNode json = mapper.readTree(message);
             String eventId = json.get("eventId").asText();
@@ -48,8 +47,6 @@ public class KafkaConsumerService {
                 log.info("Sending bet {} to RocketMQ for settlement", bet);
                 rocketMQProducer.sendBetSettlement(bet);
             }
-
-            ack.acknowledge();
         } catch (Exception e) {
             log.error("Failed to process Kafka message: {}", message, e);
             throw new RuntimeException(e);
